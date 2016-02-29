9c73f5abd310b85c5c0ffb90fbbe6ed8fae1f671
Allen <allen.hyang@hotmail.com>
2016-02-29 00:43:34 -0800

    blog dfm

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,7 +22,6 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
 )
 # Social widget
 SOCIAL = (('My sina weibo', 'http://weibo.com/u/1644145160'),
