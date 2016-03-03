df85b7a6d4c575d4ea8049bbc1d718cba384a576
Allen <allen.hyang@hotmail.com>
2016-03-02 23:46:10 -0800

    update feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Allen'
 SITENAME = u"Allen's blog"
-SITEURL = 'http://allenhyang.com'
+SITEURL = ''
 
 PATH = 'content'
 
@@ -12,16 +12,16 @@ TIMEZONE = u'Asia/Shanghai'
 
 DEFAULT_LANG = u'zh'
 # Feed generation is usually not desired when developing
-#FEED_ALL_ATOM = None
-#CATEGORY_FEED_ATOM = None
-#TRANSLATION_FEED_ATOM = None
-#AUTHOR_FEED_ATOM = None
-#AUTHOR_FEED_RSS = None
-
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-FEED_ALL_RSS = 'feeds/all.rss.xml'
+FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+#FEED_ALL_ATOM = 'feeds/all.atom.xml'
+#FEED_ALL_RSS = 'feeds/all.rss.xml'
+#CATEGORY_FEED_ATOM = None
+#TRANSLATION_FEED_ATOM = None
 
 
 # Blogroll
